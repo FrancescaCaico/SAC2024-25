e224307d24e1b3c17e20fa11ca67e4493a709de6
FrancescaCaico <82643756+FrancescaCaico@users.noreply.github.com>
2024-12-30 16:42:50 +0100

    Update topic.sh

--- template/gcloud/topic.sh
+++ template/gcloud/topic.sh
@@ -20,3 +20,13 @@ gcloud functions deploy ${FUNCTION_NAME} \
   --runtime ${RUNTIME} \
   --trigger-topic ${TOPIC_NAME} \
   --docker-registry=artifact-registry --no-gen2 --region europe-west2
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+export NAME=webuser
+export PROJECT_ID=travelscf
+gcloud iam service-accounts create ${NAME}
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member "serviceAccount:${NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --role "roles/owner"
+touch credentials.json
+gcloud iam service-accounts keys create credentials.json --iam-account ${NAME}@${PROJECT_ID}.iam.gserviceaccount.com
+export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/credentials.json"
